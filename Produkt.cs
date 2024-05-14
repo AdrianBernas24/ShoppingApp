1e31bc532cad800041c3afc4bd01bbe6ab8bc335
S1TO01 <148622261+S1TO01@users.noreply.github.com>
2024-05-14 19:58:59 +0200

    Add files via upload

---
+++ Produkt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp
+{
+    internal class Produkt
+    {
+            public string Nazwa { get; set; }
+            public decimal CenaNetto { get; set; }
+            public decimal CenaBrutto { get; set; }
+            public string KodKreskowy { get; set; }
+       
+    }
+}
